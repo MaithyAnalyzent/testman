24af2986de1bd60d501898a8e57e9747aeb3de0b
MaithyAnalyzent <maithilisaravanan05@gmail.com>
2024-12-14 08:43:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,11 +8,6 @@ class Settings(BaseSettings):
     BOT_HANDLE: str
     GROQ_API_KEY: str
     BOT_DID: Optional[str] = None
-    twitter_api_key: str
-    twitter_api_secret: str
-    twitter_access_token: str
-    twitter_access_token_secret: str
-    twitter_bearer_token: str
     emailAuthFactor: Optional[str] = None
     active: Optional[bool] = None
 
